10b19edbc15fb4e316e0fa9e305daa61bf555176
Oliver Chen <o.chen1@share.epsb.ca>
2022-08-28 13:28:27 -0600

    Better loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,10 @@
-import PulseLoader from 'react-spinners/PulseLoader';
+// import PulseLoader from 'react-spinners/PulseLoader';
 
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center bg-[#212121]">
-      <PulseLoader size={20} color={'#ededed'} loading={true} />
+    <div className="flex h-screen items-center justify-center bg-black">
+      {/* <PulseLoader size={20} color={'#ededed'} loading={true} /> */}
+      <div className="loader"></div>
     </div>
   );
 }
