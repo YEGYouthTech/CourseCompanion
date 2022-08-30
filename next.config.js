6cf5312106257ce3932b7b1ea0c455ef09c6e173
Oliver Chen <o.chen1@share.epsb.ca>
2022-08-30 16:21:54 +0000

    Update next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   poweredByHeader: false,
   trailingSlash: true,
@@ -14,4 +17,11 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 });
