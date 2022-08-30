0a58e8e667d07b53cdab8c9ed0e697b93f686316
HerilS <herilsaha06@gmail.com>
2022-08-29 21:11:30 -0600

    Matrix tables fixed

--- src/pages/app/mutuals.tsx
+++ src/pages/app/mutuals.tsx
@@ -3,23 +3,6 @@ import styled, { css } from "styled-components";
 
 import { DataContext } from "@/templates/AppMain";
 
-const Header = styled.h1<{ secondary?: boolean }>`
-  font-size: 2rem;
-  font-weight: bold;
-  margin: 0;
-  ${(props) =>
-    props.secondary &&
-    css`
-      margin-top: -0.5rem;
-      font-size: 1.1rem;
-      font-weight: normal;
-    `}
-  padding-left: 0rem;
-  @media (max-width: 850px) {
-    padding-left: 1.25rem;
-  }
-`;
-
 const TableWrapper = styled.div`
   padding-right: 3rem;
   padding-left: 3rem;
