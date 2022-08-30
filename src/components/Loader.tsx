3f9843ece37a40a6f4557a9df4bb7677890ce429
Oliver Chen <o.chen1@share.epsb.ca>
2022-08-30 10:42:04 -0600

    fix loader full width

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,5 @@
 import styles from './Loader.module.css';
 
 export default function Loader({ full = false }) {
-  return <div className={`${styles.loader} ${full & '!w-full'}`}></div>;
+  return <div className={`${styles.loader} ${full && '!w-full'}`}></div>;
 }
